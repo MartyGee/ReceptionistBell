8941183ed011d3e0f933ccff2d4613c1bafc80ea
MartyGee <martino.ghio@gmail.com>
2023-08-31 22:51:53 +0200

    Fixed groundcheck problem

--- The receptionist bell/Assets/Scripts/UnresolvedScripts/SpawnerScript.cs
+++ The receptionist bell/Assets/Scripts/UnresolvedScripts/SpawnerScript.cs
@@ -1,20 +1,25 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnerScript : MonoBehaviour
 {
-    public GameObject Object;
+    [SerializeField] public List<GameObject> objectsToSpawn;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
-            // Generate a random rotation for the object
-            Quaternion randomRotation = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+            if (objectsToSpawn.Count > 0)
+            {
+                int randomIndex = Random.Range(0, objectsToSpawn.Count);
+                GameObject objectPrefab = objectsToSpawn[randomIndex];
 
-            // Instantiate the object with the random rotation
-            Instantiate(Object, transform.position, randomRotation);
+                // Generate a random rotation for the object
+                Quaternion randomRotation = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+
+                // Instantiate the object with the random rotation
+                Instantiate(objectPrefab, transform.position, randomRotation);
+            }
         }
     }
 }
