ef262a3d872081b81160921964d0e2aa2e305ef4
MartyGee <martino.ghio@gmail.com>
2023-09-03 22:37:23 +0200

    Fixed Stage 5 button

--- The receptionist bell/Assets/Scripts/Successful tests/UIButtonInteractionScript.cs
+++ The receptionist bell/Assets/Scripts/Successful tests/UIButtonInteractionScript.cs
@@ -6,26 +6,21 @@ public class UIButtonInteractionScript : MonoBehaviour
     public Transform objectToManipulate; // Reference to the object you want to manipulate
     public Button manipulateButton; // Reference to the UI button
 
-    public Vector3 desiredPosition;
-    public Vector3 desiredRotation;
-    public Vector3 desiredScale;
+    public Vector3 desiredPosition = new Vector3(0f, 0f, 0f);
+    public Vector3 desiredRotation = new Vector3(0f, 0f, 0f); // Initial rotation, you can change this
+    public Vector3 desiredScale = new Vector3(0f, 0f, 0f);
 
     void Start()
     {
         // Assign the UI button click listener
-        manipulateButton.onClick.AddListener(SetTransform);
-
-        // Initialize the object's initial position, rotation, and scale
-        desiredPosition = objectToManipulate.position;
-        desiredRotation = objectToManipulate.rotation.eulerAngles;
-        desiredScale = objectToManipulate.localScale;
+        manipulateButton.onClick.AddListener(OnButtonClick);
     }
 
-    public void SetTransform()
+    public void OnButtonClick()
     {
         // Set the desired position, rotation, and scale when the button is clicked
         objectToManipulate.position = desiredPosition;
-        objectToManipulate.rotation = Quaternion.Euler(desiredRotation);
+        objectToManipulate.localRotation = Quaternion.Euler(desiredRotation);
         objectToManipulate.localScale = desiredScale;
     }
 }
