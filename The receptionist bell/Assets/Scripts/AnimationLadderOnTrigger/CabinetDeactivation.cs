a384a935dab0e97af4787b37010323ce1eba423f
MartyGee <martino.ghio@gmail.com>
2023-09-04 15:41:55 +0200

    Fixed collisions with cabinet

--- The receptionist bell/Assets/Scripts/AnimationLadderOnTrigger/CabinetDeactivation.cs
+++ The receptionist bell/Assets/Scripts/AnimationLadderOnTrigger/CabinetDeactivation.cs
@@ -1,13 +1,12 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class CabinetDeactivation : MonoBehaviour
 {
     public Transform objectToManipulate; // Reference to the object you want to manipulate
-    public GameObject objectToActivate1; // Reference to the GameObject to activate after animation finishes
-    public GameObject objectToActivate2; // Reference to the GameObject to activate after animation finishes
+    public List<GameObject> objectsToActivate = new List<GameObject>(); // List of GameObjects to activate after animation finishes
     public Animator objectAnimator; // Reference to the Animator component
     public GameObject objectToDeactivate; // Reference to the animated object you want to deactivate
-
 
     public Vector3 desiredPosition = new Vector3(0f, 0f, 0f);
     public Vector3 desiredRotation = new Vector3(0f, 0f, 0f); // Initial rotation, you can change this
@@ -17,22 +16,20 @@ public class CabinetDeactivation : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     public void OnAnimationComplete()
     {
         if (!animationComplete)
         {
-            // Activate the new GameObject
-            if (objectToActivate1 != null)
+            // Activate the new GameObjects from the list
+            foreach (GameObject objToActivate in objectsToActivate)
             {
-                objectToActivate1.SetActive(true);
-            }
-
-            if (objectToActivate2 != null)
-            {
-                objectToActivate2.SetActive(true);
+                if (objToActivate != null)
+                {
+                    objToActivate.SetActive(true);
+                }
             }
 
             if (objectToDeactivate != null)
